5daf74b9f55714d9d8e83d6514c5ea53d0f1ffde
Zuhair Parvez <zuhairparvez@gmail.com>
2020-12-27 13:56:08 -0800

    Personalize it to me

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Blog`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Zuhair Parvez`,
+      summary: `who lives, works, and enjoys life in the Pacific Northwest`,
     },
-    description: `A starter blog demonstrating what Gatsby can do.`,
+    description: `Zuhair Parvez's personal blog`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.app/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `zuhairp`,
     },
   },
   plugins: [
@@ -60,7 +60,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
+        name: `Zuhair Parvez's Blog`,
         short_name: `GatsbyJS`,
         start_url: `/`,
         background_color: `#ffffff`,
